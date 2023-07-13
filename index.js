9869dcd87a123d507418aecf1fe166bcc24004be
GirirajBidwai <96125918+ashish7475@users.noreply.github.com>
2023-07-13 22:43:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,14 @@ const io = new Server(server,{
 io.on("connection",(socket)=>{
     console.log("User Connected to socket "+socket.id)
 
+     socket.on("join_room",(id)=>{
+         console.log("User "+socket.id +" successfully joined room with Id "+id )
+         socket.join(id)
+     }) 
+
     socket.on("send_message",(data)=>{
-        socket.broadcast.emit("receive_message",data)
+        socket.to("123").emit("receive_message",data)
+        console.log("Message send from backend")
     })
 
     socket.on("disconnect",()=>{
